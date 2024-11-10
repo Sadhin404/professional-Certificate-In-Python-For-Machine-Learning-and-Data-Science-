0e55a3fdb396a18edf04c33992545daa43c4cea8
Sadhin khan <sadhin.pro404@gmail.com>
2024-11-10 22:11:10 +0600

    Add files via upload

---
+++ Numpy/ArrayCreationBasic.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+arzero= np.zeros((3,2))
+arones=np.ones((2,2))
+arfull=np.full((2,3),9)
+ararrange=np.arange(10)
+
+print(arzero)
+print(arones)
+print(arfull)
+print(ararrange)
---
+++ Numpy/BasicArithmaticOperation.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+ar1=np.array([1,2,3])
+ar2=np.array([4,5,6])
+
+add=np.add(ar1,ar2)
+subtract=np.subtract(ar1,ar2)
+mul=np.multiply(ar1,ar2)
+divided=np.divide(ar1,ar2)
+
+print(add, subtract, mul, divided)
